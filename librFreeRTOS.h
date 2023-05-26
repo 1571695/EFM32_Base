ce6b1a3b2a3adb855fc3d7930086d2609661b0d8
1571695 <101736521+1571695@users.noreply.github.com>
2023-05-26 13:38:48 +0200

    Fallos a resolver

---
+++ librFreeRTOS.h
@@ -0,0 +1,27 @@
+#define librFreeRTOS.h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+#include "croutine.h"
+
+#include "em_chip.h"
+#include "bsp.h"
+#include "bsp_trace.h"
+
+#include "sleep.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "em_i2c.h"
+#include "em_gpio.h"
+#include "em_cmu.h"
+
+void BSP_I2C_Init(uint8_t addr);
+bool I2C_WriteRegister(uint8_t reg, uint8_t data);
+bool I2C_ReadRegister(uint8_t reg, uint8_t *val);
+bool I2C_Test();
---
+++ main.c
@@ -0,0 +1,215 @@
+/***************************************************************************//**
+ * @file
+ * @brief FreeRTOS Blink Demo for Energy Micro EFM32GG_STK3700 Starter Kit
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2018 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#include "librFreeRTOS.h"
+
+#define STACK_SIZE_FOR_TASK    (configMINIMAL_STACK_SIZE + 10)
+#define TASK_PRIORITY          (tskIDLE_PRIORITY + 1)
+
+
+QueueHandle_t RGBab,RGB;
+
+typedef struct Message{
+	uint8_t Ra;
+	uint8_t Rb;
+	uint8_t Ga;
+	uint8_t Gb;
+	uint8_t Ba;
+	uint8_t Bb;
+} Message;
+
+typedef struct Color{
+	uint16_t R;
+	uint16_t G;
+	uint16_t B;
+} Color;
+
+int _write(int file, const char *ptr, int len) {
+    int x;
+    for (x = 0; x < len; x++) {
+       ITM_SendChar (*ptr++);
+    }
+    return (len);
+}
+
+
+
+/* Structure with parameters for LedBlink */
+typedef struct {
+  /* Delay between blink of led */
+  portTickType delay;
+  /* Number of led */
+  int          ledNo;
+} TaskParams_t;
+
+/***************************************************************************//**
+ * @brief Simple task which is blinking led
+ * @param *pParameters pointer to parameters passed to the function
+ ******************************************************************************/
+static void LedBlink(void *pParameters)
+{
+  TaskParams_t     * pData = (TaskParams_t*) pParameters;
+  const portTickType delay = pData->delay;
+
+  for (;; ) {
+    BSP_LedToggle(pData->ledNo);
+    vTaskDelay(delay);
+  }
+}
+
+static void display(void *pParameters){
+	for(;;){
+
+		Color colores;
+		if(xQueueReceive(RGB, &colores, 10) == pdPASS){
+			printf("RGB %d,%d,%d \n", colores.R, colores.G, colores.B);
+		}
+	}
+}
+
+static void traetement(void *pParameters){
+	//RGBab = (QueueHandle_t) pParameters;
+	//RGB = (QueueHandle_t) pParameters;
+	for(;;){
+
+		Color colores;
+
+		Message recMes;
+
+		xQueueReceive(RGBab, &recMes, 10);
+
+		colores.R = (((uint16_t) recMes.Rb << 8 | (uint16_t) recMes.Ra)*255)/65535;
+		colores.G = (((uint16_t) recMes.Gb << 8 | (uint16_t) recMes.Ga)*255)/65535;
+		colores.B = (((uint16_t) recMes.Bb << 8 | (uint16_t) recMes.Ba)*255)/65535;
+
+		xQueueSendToBack(RGB, &colores, 10);
+	}
+}
+
+static void semaphFunct(void *pParameters){
+	//RGBab = (QueueHandle_t) pParameters;
+	for(;;){
+		//Using semaphore for controlling the reading of the sensor.
+
+				//Tratamiento de la información recibida por el sensor.
+				//Reconocer componente i2c y demás
+				uint8_t i2cAddr = 0x88;
+				BSP_I2C_Init(i2cAddr);	//Reconocer dirección de módulo i2c --> device_addr
+				//uint8_t targetReg = 0;
+				//uint8_t dataReg;
+				bool trueFalse = I2C_Test(); //{	PAIR WITH 82C_TEST;
+				//bool trueFalse = I2C_ReadRegister(targetReg, &dataReg); //{	PAIR WITH 82C_TEST;
+				if(trueFalse == true){
+					//Set RGB Operating modes --> we've to set mode in B2-B0 to GREEN/RED/BlUE 101
+					//Set RGB Data Sensing range to B3 1 to get 10k colors. This implies using the 2 complete registers of each color.
+					//Set 16 bit to B4 in ADC Resolution
+
+					uint8_t regAux = 0x01;
+					uint8_t dataAux = 0x05;
+					I2C_WriteRegister(regAux, dataAux);
+
+					//Read Status Flag register and check if conversion has been done B1
+
+					regAux = 0x08;
+					Message mensaje1;
+
+
+					//Read RGB colors
+					regAux = 0x09;
+					I2C_ReadRegister(regAux, &mensaje1.Ra);
+					regAux = 0x0A;
+					I2C_ReadRegister(regAux, &mensaje1.Rb);
+					regAux = 0x0B;
+					I2C_ReadRegister(regAux, &mensaje1.Ga);
+					regAux = 0x0C;
+					I2C_ReadRegister(regAux, &mensaje1.Gb);
+					regAux = 0x0D;
+					I2C_ReadRegister(regAux, &mensaje1.Ba);
+					regAux = 0x0E;
+					I2C_ReadRegister(regAux, &mensaje1.Bb);
+
+					//Concatenate values to get 16bit color range.
+					regAux = 0x0E;
+
+					//AMessage xMessage;
+
+					//xMessage.ucMessageID = Ra;
+
+					if(xQueueSendToBack(RGBab, &mensaje1, 10) != pdPASS){
+						int cosasss = 12;
+					}
+
+					TaskParams_t     * datas = (TaskParams_t*) pParameters;
+					const portTickType delay = datas->delay;
+
+					vTaskDelay(delay);
+
+
+				}
+			}
+
+}
+
+/***************************************************************************//**
+ * @brief  Main function
+ ******************************************************************************/
+int main(void)
+{
+	/* Chip errata */
+  CHIP_Init();
+
+  /* If first word of user data page is non-zero, enable Energy Profiler trace */
+  BSP_TraceProfilerSetup();
+
+  /* Initialize LED driver */
+  BSP_LedsInit();
+  /* Setting state of leds*/
+  BSP_LedSet(0);
+  BSP_LedSet(1);
+
+  /* Initialize SLEEP driver, no callbacks are used */
+  SLEEP_Init(NULL, NULL);
+#if (configSLEEP_MODE < 3)
+  /* do not let to sleep deeper than define */
+  SLEEP_SleepBlockBegin((SLEEP_EnergyMode_t)(configSLEEP_MODE + 1));
+#endif
+
+  /* Parameters value for tasks*/
+  static TaskParams_t parametersToTask1 = { pdMS_TO_TICKS(1000), 0 };
+  static TaskParams_t parametersToTask2 = { pdMS_TO_TICKS(500), 1 };
+
+  //Creamos las colas
+  RGBab = xQueueCreate(1,sizeof(Message));
+  RGB = xQueueCreate(1,sizeof(Color));
+  int cosa = sizeof(Color);
+  cosa = sizeof(Message);
+
+
+  /*Create two task for blinking leds*/
+  xTaskCreate(LedBlink, (const char *) "LedBlink1", STACK_SIZE_FOR_TASK, &parametersToTask1, TASK_PRIORITY, NULL);
+  xTaskCreate(LedBlink, (const char *) "LedBlink2", STACK_SIZE_FOR_TASK, &parametersToTask2, TASK_PRIORITY, NULL);
+  xTaskCreate(semaphFunct, (const char *) "SemaphFunct", STACK_SIZE_FOR_TASK, NULL, TASK_PRIORITY, NULL);
+
+  xTaskCreate(traetement, (const char *) "traetement", STACK_SIZE_FOR_TASK, NULL, TASK_PRIORITY, NULL);
+  xTaskCreate(display, (const char *) "display", STACK_SIZE_FOR_TASK, NULL, TASK_PRIORITY, NULL);
+
+  /*Start FreeRTOS Scheduler*/
+  vTaskStartScheduler();
+
+
+
+  return 0;
+}
